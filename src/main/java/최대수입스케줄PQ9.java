a68894de163e1b9a739710bf8bb69ff012df61f2
xsa1113 <xsa1113@github.com>
2022-01-06 16:56:03 +0900

    aa

---
+++ src/main/java/최대수입스케줄PQ9.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class 최대수입스케줄PQ9 {
+    public static void main(String[] args) {
+        최대수입스케줄PQ9 T = new 최대수입스케줄PQ9();
+        Scanner sc = new Scanner(System.in);
+    }
+}
