78fb578794a65128cb0cf113b3920e52a39dc88f
xsa1113 <rlaxogjs1234@naver.com>
2022-01-21 19:42:00 +0900

    algo

---
+++ src/main/java/없는숫자더하기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class 없는숫자더하기 {
+	//[1,2,3,4,6,7,8,0]
+	static boolean check[] ;
+	
+	public int solution(int [] arr) {
+		int answer = 0;
+		int len = arr.length; 
+		for(int i=0; i<len; i++) {
+			// 1 
+			if(check[arr[i]] != true) {
+				check[arr[i]] = true;
+			}
+		}
+		
+		for(int i=0; i<10; i++) {
+			if(!check[i]) {
+				answer += i;
+			}
+		}
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		없는숫자더하기 T = new 없는숫자더하기();
+		Scanner sc = new Scanner(System.in);
+		int [] arr = new int[8];
+		check = new boolean[10];
+		for(int i=0; i<8; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(T.solution(arr));
+		
+	}
+
+}
