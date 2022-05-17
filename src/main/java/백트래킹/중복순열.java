b1e9c257b61ad263a5a97f1aa98dcb3d75e06016
kim <rlaxogjs1234@naver.com>
2022-05-17 21:30:41 +0900

    algo

--- src/main/java/백트래킹/중복순열.java
+++ src/main/java/백트래킹/중복순열.java
@@ -1,4 +1,23 @@
 package 백트래킹;
 
 public class 중복순열 {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        int r = 2;
+        int out[] = new int[r];
+        backtracking(arr, out, 0,0,r);
+    }
+
+    static void backtracking(int arr[], int out[], int depth, int start, int r ){
+        if(depth == r){
+            for(int num : out) System.out.print(num + " ");
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            out[depth]  = arr[i];
+            backtracking(arr, out, depth+1 , start+1, r);
+        }
+    }
 }
---
+++ src/main/java/백트래킹/중복조합.java
@@ -0,0 +1,16 @@
+package 백트래킹;
+
+public class 중복조합 {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        int r = 2;
+
+    }
+    static void backtracking(int arr[] , boolean check[], int depth, int start, int r){
+        if(){
+
+        }
+
+
+    }
+}
